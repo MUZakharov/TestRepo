5612da27869be01d56c795554748a9b099b48e29
MUZakharov <econtrol@mail.ru>
2020-04-14 08:55:07 +0300

    -

---
+++ postgres/Tables/public.long_long_long_long_long_long_long_long_long_long_long_name1.sql
@@ -0,0 +1,10 @@
+﻿SET SCHEMA 'public';
+
+CREATE TABLE long_long_long_long_long_long_long_long_long_long_long_name1 (
+    col integer
+);
+
+
+ALTER TABLE public.long_long_long_long_long_long_long_long_long_long_long_name1 OWNER TO postgres;
+
+ALTER TABLE public.long_long_long_long_long_long_long_long_long_long_long_name1 OWNER TO postgres;
