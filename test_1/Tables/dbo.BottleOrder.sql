ea6de18b949717e162410ba439e9345fdf0441fa
MUZakharov <econtrol@mail.ru>
2020-05-20 08:05:47 +0300

    -

---
+++ test_1/Tables/dbo.BottleOrder.sql
@@ -0,0 +1,15 @@
+﻿SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[BottleOrder](
+	[BottleOrder] [int] NULL,
+	[ClientID] [int] NULL,
+	[ContactName] [varchar](100) NULL,
+	[ProjectID] [int] NULL,
+	[ShipDate] [datetime2](7) NULL
+) ON [PRIMARY]
+
+GO
---
+++ test_1/Tables/dbo.test_ident.sql
@@ -0,0 +1,11 @@
+﻿SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[test_ident](
+	[col2] [varchar](10) NULL
+) ON [PRIMARY]
+
+GO
